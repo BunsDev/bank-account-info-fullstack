cb0e2b77230d5a9a12ace5d079038682d7d47403
Hari Pandey <69706767+Harry-code21@users.noreply.github.com>
2022-08-26 12:03:44 +0545

    contract updated till point 5

--- contracts/bankinfo.sol
+++ contracts/bankinfo.sol
@@ -1,114 +1,110 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.9;  
 
-contract bankinfo {
+contract bankinfo{
+
+
     struct bankaccount {
-        uint256 bank_name;
+        uint bank_name;
         string branch;
-        uint256 balance;
+        uint balance;
         bool exists;
     }
 
-    struct userdata {
-        uint256 accNumber;
-        uint256 bank_name;
+    struct userdata{
+        uint accNumber;
+        uint bank_name;
         string branch;
-        uint256 balance;
+        uint balance;
+ 
     }
 
     struct bankdata {
-        uint256 accNumber;
+        uint accNumber;
         string branch;
-        uint256 balance;
+        uint balance;
     }
-    uint256[] no;
-    uint256[] name;
-    string[] brh;
-    uint256[] balc;
+ 
+    uint[] acNumbercount;   // array storing account numbers 
+    address[] addresses;     // array storing addresses 
 
-    uint256[] acNumbercount; // array storing account numbers
-    address[] addresses; // array storing addresses
-
-    userdata[] public arrayuserdata;
+    userdata[] public arrayuserdata;   
     bankdata[] public arraybankdata;
 
-    mapping(address => mapping(uint256 => bankaccount))
-        public mappedbankaccount;
+    mapping(address=>mapping(uint=>bankaccount)) public mappedbankaccount;
 
-    function setInfo(
-        uint256 _number,
-        uint256 _bank,
-        string memory _branch,
-        uint256 _balance
-    ) public {
-        mappedbankaccount[msg.sender][_number] = bankaccount(
-            _bank,
-            _branch,
-            _balance,
-            true
-        );
+    function setInfo(uint _number,uint _bank,string memory _branch,uint _balance) public 
+    {
+        mappedbankaccount[msg.sender][_number]=bankaccount(_bank,_branch,_balance,true);
         acNumbercount.push(_number);
         addresses.push(msg.sender);
-        no.push(_bank);
-        name.push(_bank);
-        brh.push(_branch);
-        balc.push(_balance);
+     
+         
     }
 
-    function deposit(uint256 _number, uint256 _balance) public {
-        mappedbankaccount[msg.sender][_number].balance += _balance;
+
+
+    function deposit(uint _number,uint _balance) public 
+    {
+        mappedbankaccount[msg.sender][_number].balance+=_balance;   
     }
 
-    function withdraw(uint256 _number, uint256 _balance) public {
-        mappedbankaccount[msg.sender][_number].balance -= _balance;
+
+     function withdraw(uint _number,uint _balance) public 
+    {
+        mappedbankaccount[msg.sender][_number].balance-=_balance;   
     }
 
-    function getDataOfUser() public view returns (userdata[] memory) {
-        userdata[] memory muserdata = new userdata[](acNumbercount.length);
+    function getDataOfUser() public view returns(userdata[] memory )
+    {
+        
+        userdata[] memory muserdata= new userdata[](acNumbercount.length);
 
-        for (uint256 i = 0; i < acNumbercount.length; i++) {
-            if (
-                mappedbankaccount[msg.sender][acNumbercount[i]].exists == true
-            ) {
-                userdata memory newuserdata = userdata(
-                    acNumbercount[i],
-                    no[i],
-                    brh[i],
-                    balc[i]
-                );
-                muserdata[i] = newuserdata;
+        for(uint i=0;i<acNumbercount.length;i++)
+        {
+            if(mappedbankaccount[msg.sender][acNumbercount[i]].exists == true){
+            userdata memory newuserdata=userdata(acNumbercount[i],mappedbankaccount[msg.sender][acNumbercount[i]].bank_name,mappedbankaccount[msg.sender][acNumbercount[i]].branch,mappedbankaccount[msg.sender][acNumbercount[i]].balance);
+            muserdata[i]=newuserdata;
+
+          
+            
             }
         }
         return muserdata;
+        
     }
 
-    function getDataOfBank(uint256 _name)
-        public
-        view
-        returns (bankdata[] memory)
+    function getDataOfBank(uint _name) public view returns(bankdata[] memory )
     {
-        uint256 count;
-        bankdata[] memory mbankdata = new bankdata[](acNumbercount.length); // new array to store struct
+        uint count;
+        bankdata[] memory mbankdata= new bankdata[](acNumbercount.length); // new array to store struct 
 
-        for (uint256 i = 0; i < addresses.length; i++) {
-            for (uint256 j = 0; j < acNumbercount.length; j++) {
-                if (
-                    mappedbankaccount[addresses[i]][acNumbercount[j]]
-                        .bank_name == _name
-                ) {
-                    // new struct to store data
-                    bankdata memory newbankdata = bankdata(
-                        acNumbercount[j],
-                        mappedbankaccount[addresses[i]][acNumbercount[j]]
-                            .branch,
-                        mappedbankaccount[addresses[i]][acNumbercount[j]]
-                            .balance
-                    );
-                    mbankdata[i] = newbankdata;
-                    count++;
+        for(uint i=0;i<addresses.length;i++)
+        {
+            for(uint j=0;j<acNumbercount.length;j++)
+            {
+                if(mappedbankaccount[addresses[i]][acNumbercount[j]].bank_name ==_name)
+                {
+                    // new struct to store data 
+                bankdata memory newbankdata=bankdata(acNumbercount[j],mappedbankaccount[addresses[i]][acNumbercount[j]].branch,mappedbankaccount[addresses[i]][acNumbercount[j]].balance);
+                mbankdata[i]=newbankdata;
+                count++;
                 }
-            }
+            }        
         }
         return mbankdata;
+        
     }
+
+    function tranferAmount(uint _fromAccountNo,uint _balance,address _to,uint _toAccountNo) public 
+    {
+        mappedbankaccount[_to][_toAccountNo].balance+=_balance;
+        mappedbankaccount[msg.sender][_fromAccountNo].balance-=_balance;
+
+    }    
+
 }
+
+
+
+
