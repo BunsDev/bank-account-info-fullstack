12e3200ed612c7fc332b8470e831c87dcc7a9a19
Hari Pandey <69706767+Harry-code21@users.noreply.github.com>
2022-08-26 17:00:11 +0545

    signer attached

--- README.md
+++ README.md
@@ -10,4 +10,16 @@ npx hardhat test
 GAS_REPORT=true npx hardhat test
 npx hardhat node
 npx hardhat run scripts/deploy.js
+
+------------------------------------------------------------------------
+methods calling : 
+ // const[addr1,addr22,addr3]=await ethers.getSigners();
+
+ // await deployedbankinfoContract.connect(addr2).setInfo(1,11,"nic",1200);
+ // await deployedbankinfoContract.deposit(1,1000);
+ // getdata=await deployedbankinfoContract.getDataOfUser();
+ // console.log(getdata);
+ // balance=await deployedbankinfoContract.getBalance(1);
+ // console.log(balance);
+  //await deployedbankinfoContract.transferAmount(1,2500,addr2.address,2);
 ```
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -4,28 +4,17 @@ async function main() {
   const bankinfoContract  = await ethers.getContractFactory("bankinfo");
   const deployedbankinfoContract  = await bankinfoContract.deploy();
   await deployedbankinfoContract.deployed();
-  // const[addr1,addr22,addr3]=await ethers.getSigners();
-
-  await deployedbankinfoContract.setInfo(1,11,"nic",1200);
-  await deployedbankinfoContract.deposit(1,1000);
-  getdata=await deployedbankinfoContract.getDataOfUser();
-  console.log(getdata);
-  balance=await deployedbankinfoContract.getBalance(1);
-  console.log(balance);
-  await deployedbankinfoContract.transferAmount(1,2500,addr2.address,2);
+ 
 
   
-  console.log("datas :",res);
-  
-  console.log
   
   
-  (
+  console.log(
     "deployed Contract Address:",deployedbankinfoContract.address );
 }
 
 main()
-.then(() => process.exit(0))
+ .then(() => process.exit(0))
   .catch((error) => {
     console.error(error);
     process.exit(1);
