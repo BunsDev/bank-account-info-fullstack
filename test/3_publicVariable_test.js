d2c97997c1db11f9f63d556cdd975e057b8febfb
Hari Pandey <69706767+Harry-code21@users.noreply.github.com>
2022-08-29 15:33:02 +0545

    public variable testing

---
+++ test/3_publicVariable_test.js
@@ -0,0 +1,20 @@
+const { expect } = require("chai");
+const hre = require("hardhat");
+
+describe("Public Variable testing ", function () {
+  it("Should return owner address as variable 'owner' is made public  ", async function () {
+
+    const instancebankinfo = await hre.ethers.getContractFactory("bankinfo");
+    const deployedbankinfo = await instancebankinfo.deploy();
+
+     ownerAdrress=await deployedbankinfo.owner();
+    console.log("owneraddress :",ownerAdrress);
+
+
+  });
+ 
+ 
+})
+
+
+
