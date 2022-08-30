ff93c20baa95b5e2dfff82bded696188141ce431
Hari Pandey <69706767+Harry-code21@users.noreply.github.com>
2022-08-30 09:07:22 +0545

    contract enhancement

--- contracts/bankinfo.sol
+++ contracts/bankinfo.sol
@@ -22,8 +22,8 @@ contract bankinfo {
         uint256 balance;
     }
     address public owner;
-    constructor()
-    {
+
+    constructor() {
         owner = msg.sender;
     }
 
@@ -36,56 +36,55 @@ contract bankinfo {
     mapping(address => mapping(uint256 => bankaccount))
         public mappedbankaccount;
 
-/*----------------------------------modidiers---------------------------------------------*/
-modifier accountAlreadyExist(uint256 _acNumber) {
-		bool check;
-		for (uint256 i = 0; i < accounts.length; i++) {
-			if (accounts[i] == _acNumber) {
-				check = true;
-				break;
-			}
-		}
-		require(check== false, "Account already exists,please Change your Account Number");
-		_;
-	}
-
+    /*----------------------------------modidiers---------------------------------------------*/
+    modifier accountAlreadyExist(uint256 _acNumber) {
+        bool check;
+        for (uint256 i = 0; i < accounts.length; i++) {
+            if (accounts[i] == _acNumber) {
+                check = true;
+                break;
+            }
+        }
+        require(
+            check == false,
+            "Account already exists,please Change your Account Number"
+        );
+        _;
+    }
 
     modifier isAccountExist(address _accountAddress, uint256 _accountNumber) {
-		require(
-			mappedbankaccount[_accountAddress][_accountNumber].exists == true,
-			"Account number doesnot exists"
-		);
-		_;
-	}
+        require(
+            mappedbankaccount[_accountAddress][_accountNumber].exists == true,
+            "Account number doesnot exists"
+        );
+        _;
+    }
 
-    	/*--------------------------Events------------------------------------------*/
+    /*--------------------------Events------------------------------------------*/
 
-	event eventAccountCreation(address accountAddress, uint accountNumber);
+    event eventAccountCreation(address accountAddress, uint256 accountNumber);
 
+    event eventBalanceDeposit(
+        address accountAddress,
+        uint256 accountNumber,
+        uint256 depositedBalance
+    );
 
-	event eventBalanceDeposit(
-		address accountAddress,
-		uint accountNumber,
-		uint depositedBalance
-	);
+    event eventBalanceTransfer(
+        uint256 accountFrom,
+        uint256 accountTo,
+        uint256 transferedBalance
+    );
 
+    event eventBalanceWithdraw(
+        address accountAddress,
+        uint256 accountNumber,
+        uint256 withdrawlBalance
+    );
 
-    	event eventBalanceTransfer(
-		uint accountFrom,
-		uint accountTo,
-		uint transferedBalance
-	);
+    event eventAdminChange(address newAdmin);
 
-
-	event eventBalanceWithdraw(
-		address accountAddress,
-		uint accountNumber,
-		uint withdrawlBalance
-	);
-
-	event eventAdminChange(address newAdmin);
-
-/*----------------------------------------functions -------------------------------------------------*/
+    /*----------------------------------------functions -------------------------------------------------*/
     function setInfo(
         uint256 _accountNumber,
         uint256 _bank,
@@ -100,33 +99,32 @@ modifier accountAlreadyExist(uint256 _acNumber) {
         );
         accounts.push(_accountNumber);
         addresses.push(msg.sender);
-        emit  eventAccountCreation(msg.sender,_accountNumber);
+        emit eventAccountCreation(msg.sender, _accountNumber);
     }
 
-    function deposit(uint256 _accountNumber, uint256 _depositBalance) public  {
-        mappedbankaccount[msg.sender][_accountNumber].balance += _depositBalance;
+    function deposit(uint256 _accountNumber, uint256 _depositBalance) public {
+        mappedbankaccount[msg.sender][_accountNumber]
+            .balance += _depositBalance;
         emit eventBalanceDeposit(msg.sender, _accountNumber, _depositBalance);
     }
 
     function withdraw(uint256 _accountNumber, uint256 _withdrawBalance) public {
-        mappedbankaccount[msg.sender][_accountNumber].balance -= _withdrawBalance;
-          emit eventBalanceWithdraw(msg.sender, _accountNumber, _withdrawBalance);
-
+        mappedbankaccount[msg.sender][_accountNumber]
+            .balance -= _withdrawBalance;
+        emit eventBalanceWithdraw(msg.sender, _accountNumber, _withdrawBalance);
     }
 
     function getDataOfUser() public view returns (userdata[] memory) {
-        userdata[] memory muserdata = new userdata[](accounts.length);  //new array
+        userdata[] memory muserdata = new userdata[](accounts.length); //new array
 
         for (uint256 i = 0; i < accounts.length; i++) {
-            if (
-                mappedbankaccount[msg.sender][accounts[i]].exists == true
-            ) {
+            if (mappedbankaccount[msg.sender][accounts[i]].exists == true) {
                 userdata memory newuserdata = userdata(
                     accounts[i],
                     mappedbankaccount[msg.sender][accounts[i]].bank_name,
                     mappedbankaccount[msg.sender][accounts[i]].branch,
                     mappedbankaccount[msg.sender][accounts[i]].balance
-                );  // new struct to store data 
+                ); // new struct to store data
                 muserdata[i] = newuserdata;
             }
         }
@@ -144,16 +142,14 @@ modifier accountAlreadyExist(uint256 _acNumber) {
         for (uint256 i = 0; i < addresses.length; i++) {
             for (uint256 j = 0; j < accounts.length; j++) {
                 if (
-                    mappedbankaccount[addresses[i]][accounts[j]]
-                        .bank_name == _name
+                    mappedbankaccount[addresses[i]][accounts[j]].bank_name ==
+                    _name
                 ) {
                     // new struct to store data
                     bankdata memory newbankdata = bankdata(
                         accounts[j],
-                        mappedbankaccount[addresses[i]][accounts[j]]
-                            .branch,
-                        mappedbankaccount[addresses[i]][accounts[j]]
-                            .balance
+                        mappedbankaccount[addresses[i]][accounts[j]].branch,
+                        mappedbankaccount[addresses[i]][accounts[j]].balance
                     );
                     mbankdata[i] = newbankdata;
                     count++;
@@ -168,14 +164,20 @@ modifier accountAlreadyExist(uint256 _acNumber) {
         uint256 _transferBalance,
         address _toAddress,
         uint256 _toAccountNo
-    ) public isAccountExist(_toAddress,_transferBalance) {
+    ) public isAccountExist(_toAddress, _transferBalance) {
         require(
-            mappedbankaccount[msg.sender][_fromAccountNo].balance >= _transferBalance,
+            mappedbankaccount[msg.sender][_fromAccountNo].balance >=
+                _transferBalance,
             "insufficient balance"
         );
         mappedbankaccount[_toAddress][_toAccountNo].balance += _transferBalance;
-        mappedbankaccount[msg.sender][_fromAccountNo].balance -= _transferBalance;
-        emit eventBalanceTransfer(_fromAccountNo, _toAccountNo, _transferBalance);
+        mappedbankaccount[msg.sender][_fromAccountNo]
+            .balance -= _transferBalance;
+        emit eventBalanceTransfer(
+            _fromAccountNo,
+            _toAccountNo,
+            _transferBalance
+        );
     }
 
     function getBalance(uint256 _acno) public view returns (uint256) {
@@ -183,8 +185,11 @@ modifier accountAlreadyExist(uint256 _acNumber) {
     }
 
     function transferOwner(address _newAdminaddr) public {
-        require(msg.sender == owner,"Permission denied as you are not owner of this Account");
-        owner =_newAdminaddr;
+        require(
+            msg.sender == owner,
+            "Permission denied as you are not owner of this Account"
+        );
+        owner = _newAdminaddr;
         emit eventAdminChange(_newAdminaddr);
     }
 }
